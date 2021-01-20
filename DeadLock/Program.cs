b4b9958c92160adb0f1e08c27b9b8b6fa1c5c092
shmykov.alexander <shmykov.alexander@gmail.com>
2021-01-20 19:08:46 +0300

    поправил удалил файлы

--- DeadLock/Program.cs
+++ DeadLock/Program.cs
@@ -12,31 +12,45 @@ namespace DeadLock
             while (!File.Exists(file1))
             {
                 Thread.Sleep(sleepTimeMs);
-                Console.WriteLine($"file{file1} not exist! waiting creation");
+                Console.WriteLine($"file {file1} not exist! waiting creation");
             }
 
             using (var f = File.Create(file2))
             {
             } ;
         }
+
+        static void DeleteFileIfExist(string[]  fileNames)
+        {
+            foreach (string fileName in fileNames)
+            {
+                if (File.Exists(fileName)) File.Delete(fileName);
+            }
+            
+        }
+
         static async Task Main(string[] args)
         {
-            
+            var aTxt = "a.txt";
+            var bTxt = "b.txt";
+            string[] ab = new string[] { aTxt, bTxt };
+            DeleteFileIfExist(ab);
+            if (File.Exists(bTxt))File.Delete(bTxt);
             var t1= Task.Run(() =>{
-                FileCreationDeadlockAsync("a.txt", "b.txt", 100);
+                FileCreationDeadlockAsync(aTxt, bTxt, 100);
             });
             var t2 = Task.Run(() => {
-                FileCreationDeadlockAsync("a.txt", "b.txt", 100);
+                FileCreationDeadlockAsync(bTxt, aTxt, 100);
             });
             var ch = Console.ReadKey();
-            await using (var f = File.Create("a.txt"))
+            await using (var f = File.Create(aTxt))
             {
             };
            
-            var ch2 = Console.ReadKey();
             await t1;
             await t2;
-
+            var ch2 = Console.ReadKey();
+            DeleteFileIfExist(ab);
 
 
 
